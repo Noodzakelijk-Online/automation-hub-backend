bb9881a9cf1cf1fd1094e8831e78af6e304835cf
Jackson Barreto <jackson.barreto@gmail.com>
2023-09-16 18:30:07 +0200

    feat: database

---
+++ internal/infra/database.go
@@ -0,0 +1,42 @@
+package infra
+
+import (
+	"automation-hub-backend/internal/config"
+	"automation-hub-backend/internal/model"
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewPostgresDatabase(user, password, dbName, dbHost string, dbPort int) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=UTC",
+		dbHost, user, password, dbName, dbPort)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func GetDefaultDB() (*gorm.DB, error) {
+	db, err := NewPostgresDatabase(config.Config.DbUser, config.Config.DbPassword,
+		config.Config.DbName, config.Config.DbHost, config.Config.DbPort)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := RunMigrations(db); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func RunMigrations(db *gorm.DB) error {
+	if err := db.AutoMigrate(&model.Automation{}); err != nil {
+		return err
+	}
+	return nil
+}
